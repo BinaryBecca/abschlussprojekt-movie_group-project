f3a54447b19565726cd7e940939647e24a0fc490
BinaryBecca <rebecca.thinnes@web.de>
2025-09-23 16:38:31 +0200

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router"
 import "./App.css"
+import Layout from "./layout/Layout"
+import HomePage from "./pages/homePage/HomePage"
+import StartPage from "./pages/startPage/StartPage"
+import LoginPage from "./pages/loginPage/LoginPage"
+import TrendingMoviesPage from "./pages/trendingMoviesPage/TrendingMoviesPage"
+import GenresPage from "./pages/genresPage/GenresPage"
+import DetailsPage from "./pages/detailsPage/DetailsPage"
 
 // const myAPI = import.meta.env.VITE_apiKey
 
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="start" element={<StartPage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="trending" element={<TrendingMoviesPage />} />
+        <Route path="genres" element={<GenresPage />} />
+        <Route path="details/:id" element={<DetailsPage />} />
+      </Route>
+    )
+  )
   return (
     <>
-      <div></div>
+      <RouterProvider router={router} />
     </>
   )
 }
