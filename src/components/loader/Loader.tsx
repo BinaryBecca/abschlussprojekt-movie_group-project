5ae8d1f17e4c85480f221cf175beb8f4299d9ce6
riya <riya.ueng@gmail.com>
2025-09-24 15:57:22 +0200

    loader_startscreen_animation_created

---
+++ src/components/loader/Loader.tsx
@@ -0,0 +1,11 @@
+export default function Loader() {
+  return (
+    <>
+      <section className="flex justify-center items-center h-screen bg-[#181732]">
+        <div className="loading">
+          <img src="../../../public/img/logo_mov.svg" alt="Loading Logo" className="w-15" />
+        </div>
+      </section>
+    </>
+  )
+}
