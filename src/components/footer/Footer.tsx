b8a2f6ef43e6b142042c0e372dad10fd0c65decf
riya <riya.ueng@gmail.com>
2025-09-24 14:50:31 +0200

    navbuttons_and_navbar_added

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,21 +4,18 @@ import NavButton from "../navButton/NavButton"
 
 export default function Footer() {
   return (
-    <footer className=" p-4 mt-auto">
-      <nav className="flex justify-center gap-2">
-        <button className="p-4 rounded bg-[#4DA895] text-white">Lorem</button>
-        <button className="p-4 rounded bg-[#4DA895] text-white">Lorem</button>
-        <button className="p-4 rounded bg-[#4DA895] text-white">Lorem</button>
-        <button className="p-4 rounded bg-[#4DA895] text-white">Lorem</button>
-        <button className="p-4 rounded bg-[#4DA895] text-white">Lorem</button>
-      </nav>
-
-      <nav>
-        <NavButton link="/" img="/" text="Home" className="" />
-        <NavButton link="/favorites" img="" className="" />
-        <NavButton link="/download" img="" className="" />
-        <NavButton link="/favorites" img="" className="" />
-        <NavButton link="/profile" img="" className="" />
+    <footer className="mt-auto">
+      <nav className="flex justify-between items-center p-10">
+        <NavButton
+          link="/"
+          img="../../../public/img/icon_home.svg"
+          text="Home"
+          classNameImg="h-8"
+          className="flex items-center justify-center gap-2"
+        />
+        <NavButton link="/favorites" img="../../../public/img/icon_favorites.svg" className="" classNameImg="h-8" />
+        <NavButton link="/download" img="../../../public/img/icon_download.svg" className="" classNameImg="h-8" />
+        <NavButton link="/profile" img="../../../public/img/icon_profile.svg" className="" classNameImg="h-8" />
       </nav>
     </footer>
   )
