44e8774dfbfcc7097d906285249bb2e9af5360f9
BinaryBecca <rebecca.thinnes@web.de>
2025-09-24 17:13:32 +0200

    update trendingMoviesPage

--- src/pages/trendingMoviesPage/TrendingMoviesPage.tsx
+++ src/pages/trendingMoviesPage/TrendingMoviesPage.tsx
@@ -1,10 +1,3 @@
-import MovieList from "../../components/movieList/MovieList"
-import type { IMovieDetails } from "../../interfaces/IMovieDetails"
-
-interface TrendingMoviesPageProps {
-  movies: IMovieDetails[]
-}
-
-export default function TrendingMoviesPage({ movies }: TrendingMoviesPageProps) {
-  return <>{/* <MovieList movies={movies} /> */}</>
+export default function TrendingMoviesPage() {
+  return <div>TrendingMoviesPage</div>
 }
