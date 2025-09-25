8c183699ab5e85ba58c2bfd051550e66831390cd
BinaryBecca <rebecca.thinnes@web.de>
2025-09-25 14:18:51 +0200

    trendingPage, Movielist, MovieCards und link to details

--- src/components/movieCard/MovieCard.tsx
+++ src/components/movieCard/MovieCard.tsx
@@ -1,4 +1,6 @@
+import { Link } from "react-router"
 import type { IMovieDetails } from "../../interfaces/IMovieDetails"
+import NavButton from "../navButton/NavButton"
 
 interface MovieCarouselCardProps {
   movie: IMovieDetails
@@ -11,18 +13,29 @@ export default function MovieCard({ movie }: MovieCarouselCardProps) {
   const roundedVoteAverage = Number(movie.vote_average).toFixed(1)
 
   return (
-    <section>
-      <img src={frontImg} alt={movie.title} />
+    <section className=" border border-white m-5">
+      <div className="grid grid-cols-[1fr_2fr] p-5 items-center">
+        <Link to={`/details/${movie.id}`}>
+          <img className="rounded-4xl h-auto max-h-[20em]" src={frontImg} alt={movie.title} />
+        </Link>
 
-      <div>
-        <div>
-          <h4 className="text-2xl font-bold">{movie.title}</h4>
-        </div>
-        <div>
-          <p className="font-light">⭐ {roundedVoteAverage}/ 10.0</p>
-          <p className="font-light">{movie.release_date}</p>
-          {/* <p className="font-light">{movie.genres.name}</p> */}
-          <p className="font-light">{movie.runtime} min</p>
+        <div className="flex flex-col gap-10">
+          <div className="flex flex-row justify-between items-center">
+            <h4 className="text-2xl font-bold">{movie.title}</h4>
+            <NavButton link="/favorites" img="/icon_download.svg" className="" classNameImg="h-8" />
+          </div>
+
+          <div className="flex flex-row justify-between items-center font-light">
+            <p>⭐ {roundedVoteAverage} </p>
+            <p>●</p>
+            <p>{movie.release_date.slice(0, 4)}</p>
+            <p>●</p>
+            <div className="flex flex-row">
+              <p>{movie.genres?.map((genre) => genre.name).join(" | ")}</p>
+            </div>
+            <p>●</p>
+            <p>{movie.runtime} min</p>
+          </div>
         </div>
       </div>
     </section>
--- src/pages/trendingMoviesPage/TrendingMoviesPage.tsx
+++ src/pages/trendingMoviesPage/TrendingMoviesPage.tsx
@@ -1,12 +1,34 @@
+import { useEffect, useState } from "react"
 import MovieList from "../../components/movieList/MovieList"
 import useMovies from "../../functions/Functions"
+import type { IMovieDetails } from "../../interfaces/IMovieDetails"
+import type { Result } from "../../interfaces/ITrendingMovies"
 
 export default function TrendingMoviesPage() {
-  const { trending } = useMovies()
+  const { trending, fetchDetailedMovie } = useMovies()
+  const [moreMovies, setMoreMovies] = useState<IMovieDetails[]>([])
+
+  useEffect(() => {
+    async function loadDetails() {
+      const show50Movies: Result[] = trending.slice(0, 50)
+      const showMoreMovies = await Promise.all(
+        show50Movies.map((movie: Result) => {
+          if (movie.id) {
+            return fetchDetailedMovie(movie.id)
+          } else {
+            return null
+          }
+        })
+      )
+      setMoreMovies(showMoreMovies)
+    }
+
+    loadDetails()
+  }, [trending])
 
   return (
     <div>
-      <MovieList movies={trending} />
+      <MovieList movies={moreMovies} />
     </div>
   )
 }
