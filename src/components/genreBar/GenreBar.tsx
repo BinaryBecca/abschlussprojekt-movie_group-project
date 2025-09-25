684c8038acc26e6047e2c468c9e7175a604f571b
Johnstb <jstaub8596@gmail.com>
2025-09-25 10:23:24 +0200

    Genre Bar Sliding Funktion geschrieben und function zum anzeigen von einer bestimmten anzahl von buttons

--- src/components/genreBar/GenreBar.tsx
+++ src/components/genreBar/GenreBar.tsx
@@ -1,9 +1,12 @@
-import React, { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import useMovies from '../../functions/Functions'
 import MovieButton from '../movieButton/MovieButton'
+import { Carousel } from 'react-bootstrap'
+import type { Genre } from '../../interfaces/IGenres'
 
 export default function GenreBar() {
   const { genres, fetchGenreNavBar, fetchGenreByTrend, loading } = useMovies()
+  const [index, setIndex] = useState(0)
 
   // ! Genres laden nur einmal (wenn noch keine da sind)
   useEffect(() => {
@@ -12,16 +15,49 @@ export default function GenreBar() {
 
   if (loading && genres.length === 0) return <div>Lade Genres...</div>
 
+  // Hilfsfunktion: Genres in 3 Buttons pro Slide aufteilen
+  const splittedGenres: Genre[][] = []
+  for (let i = 0; i < genres.length; i += 3) {
+    splittedGenres.push(genres.slice(i, i + 3))
+  }
+
   return (
     <div>
-      {genres.map((g) => (
-        <MovieButton
-          key={g.id}
-          genre={g.name}
-          className="px-4 py-2 bg-gray-200 hover:bg-gray-300 rounded"
-          onClick={() => fetchGenreByTrend(g.id)}
-        />
-      ))}
+      <Carousel
+        activeIndex={index}
+        onSelect={(i) => setIndex(i)}
+        interval={null} // kein Auto-Slide
+        indicators={false} // optional
+        prevIcon={
+          <span
+            className="inline-block text-2xl text-red-400 font-bold" // Custom styling des Slider Icons
+          >
+            ‹
+          </span>
+        }
+        nextIcon={
+          <span
+            className="inline-block text-2xl text-red-400 font-bold" // Custom styling des Slider Icons
+          >
+            ›
+          </span>
+        }
+      >
+        {splittedGenres.map((group, index) => (
+          <Carousel.Item key={index}>
+            <div className="d-flex justify-content-center gap-2 py-2">
+              {group.map((g) => (
+                <MovieButton
+                  key={g.id}
+                  genre={g.name}
+                  className="px-4 py-2 bg-gray-200 hover:bg-gray-300 rounded"
+                  onClick={() => fetchGenreByTrend(g.id)}
+                />
+              ))}
+            </div>
+          </Carousel.Item>
+        ))}
+      </Carousel>
     </div>
   )
 }
