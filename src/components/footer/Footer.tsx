a2fb59bfafec47c9814399c500fe1d03f4aba029
riya <riya.ueng@gmail.com>
2025-09-25 17:30:23 +0200

    navbar_color_change

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,7 +2,7 @@ import NavButton from "../navButton/NavButton"
 
 export default function Footer() {
   return (
-    <footer className="fixed bottom-0 left-0 right-0 z-50 bg-darkblue ">
+    <footer className="fixed bottom-0 left-0 right-0 z-50 bg-[#39395A] ">
       <nav className="flex justify-between items-center px-7 py-3">
         <NavButton
           link="/"
