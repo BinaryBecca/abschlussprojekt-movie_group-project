908f3def37ccb3d902c0d84a627892671a5c6f1b
riya <riya.ueng@gmail.com>
2025-09-25 11:25:09 +0200

    main_wrapper_in_app.tsx

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,28 +1,28 @@
-import { Outlet } from 'react-router'
-import Footer from '../components/footer/Footer'
-import Header from '../components/header/Header'
-import Loader from '../components/loader/Loader'
-import useMovies from '../functions/Functions'
-import StartScreen from '../components/startScreen/StartScreen'
+import { Outlet } from "react-router"
+import Footer from "../components/footer/Footer"
+import Header from "../components/header/Header"
+import Loader from "../components/loader/Loader"
+import useMovies from "../functions/Functions"
+import StartScreen from "../components/startScreen/StartScreen"
 
 export default function Layout() {
   const { displayScreen, setDisplayScreen } = useMovies()
 
-  if (displayScreen === 'loading') {
+  if (displayScreen === "loading") {
     return <Loader />
   }
 
-  if (displayScreen === 'start') {
-    return <StartScreen onClick={() => setDisplayScreen('home')} />
+  if (displayScreen === "start") {
+    return <StartScreen onClick={() => setDisplayScreen("home")} />
   }
 
   return (
-    <>
-      <>
-        <Header />
+    <div className="bg-darkblue">
+      <Header />
+      <main>
         <Outlet />
-        <Footer />
-      </>
-    </>
+      </main>
+      <Footer />
+    </div>
   )
 }
