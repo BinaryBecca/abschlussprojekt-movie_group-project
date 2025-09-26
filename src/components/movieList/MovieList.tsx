dd59c28bf71cec9067e53a88360e72d70d125cd3
BinaryBecca <rebecca.thinnes@web.de>
2025-09-26 09:42:14 +0200

    navigate backbutton

--- src/components/movieList/MovieList.tsx
+++ src/components/movieList/MovieList.tsx
@@ -1,6 +1,5 @@
-import { Link } from "react-router"
+import { useNavigate } from "react-router"
 import type { IMovieDetails } from "../../interfaces/IMovieDetails"
-import type { Result } from "../../interfaces/ITrendingMovies"
 import MovieCard from "../movieCard/MovieCard"
 
 interface MovieListProps {
@@ -9,13 +8,14 @@ interface MovieListProps {
 }
 
 export default function MovieList({ movies, title = "Movies" }: MovieListProps) {
+  const navigate = useNavigate()
   return (
     <>
       <section className="px-6">
-        <Link to={`/`} className=" py-1 flex items-center gap-2 !no-underline mb-3">
+        <button onClick={() => navigate(-1)} className=" py-1 flex items-center gap-2 !no-underline mb-3">
           <img src="/img/icon_arrow.svg" alt="Icon Arrow" className="w-5" />{" "}
           <p className="text-green size-2 text-[1.1rem] hover:text-lightgreen ">Back</p>
-        </Link>
+        </button>
         {title && (
           <div className="mb-4">
             <h1>{title}</h1>
--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -10,7 +10,7 @@ export default function HomePage() {
   useEffect(() => {
     setClickedOnSearchButton(false)
     fetchTrendingMovies()
-  }, [])
+  }, [setClickedOnSearchButton])
 
   return (
     <>
