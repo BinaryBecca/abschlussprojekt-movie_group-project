0d141acca3ca9a29acf39f7e7abcc35038a7d010
BinaryBecca <rebecca.thinnes@web.de>
2025-09-26 07:55:27 +0200

    IMovieetails in MovieCards & MovieList

--- src/components/movieList/MovieList.tsx
+++ src/components/movieList/MovieList.tsx
@@ -1,28 +1,21 @@
-import type { IMovieDetails } from '../../interfaces/IMovieDetails'
-import type { Result } from '../../interfaces/ITrendingMovies'
-import MovieCard from '../movieCard/MovieCard'
+import type { IMovieDetails } from "../../interfaces/IMovieDetails"
+import MovieCard from "../movieCard/MovieCard"
 
 interface MovieListProps {
-  movies: Result[]
-  title?: string
+  movies: IMovieDetails[]
 }
 
-export default function MovieList({
-  movies,
-  title = 'Movies',
-}: MovieListProps) {
+export default function MovieList({ movies }: MovieListProps) {
   return (
     <>
       <section>
-        {title && (
-          <div className="mb-4">
-            <h1>{title}</h1>
-          </div>
-        )}
+        <div className="mb-4">
+          <h1>Trending Movies</h1>
+        </div>
         <ul className="list-none p-0">
           {movies.map((movie) => {
             return (
-              <li key={movie.id}>
+              <li key={movie.id} className="">
                 <MovieCard movie={movie} />
               </li>
             )
